cfd752a61c18aff7a8a128afc5d1a6438207c3b3
rowan-jansens <67484064+rowan-jansens@users.noreply.github.com>
2021-10-27 11:06:39 -0400

    added thermostat function and demo

---
+++ get_heater_state.m
@@ -0,0 +1,18 @@
+function heater_state = get_heater_state(temp, set_temp, heater_state)
+    buffer = 1;
+    %thermostat logic
+    %if heater is off and house is colder than minimum tolerance...
+    if temp < (set_temp - buffer)
+        %turn heater on
+        heater_state = true;
+    end
+
+    %if heater is heating, continue heating untill maximum tolerance is reached
+    if temp < (set_temp + buffer) && heater_state == true
+        heater_state = true;
+        %if temperature is at maximum tolerance
+    elseif temp > (set_temp - buffer)
+        %turn off the heater
+        heater_state = false;
+    end
+end
