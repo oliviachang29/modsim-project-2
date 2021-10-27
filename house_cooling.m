18b4110843ad5215013c62bd907f70786d615c22
rowan-jansens <67484064+rowan-jansens@users.noreply.github.com>
2021-10-27 17:46:58 -0400

    tried some new stuff

--- house_cooling.m
+++ house_cooling.m
@@ -1,6 +1,6 @@
 function [time, TEMP] = house_cooling()
 
-    set_temp = 298;
+    set_temp = 293;
     
     temp_amb = 270;
     heater_state = true;
@@ -8,16 +8,20 @@ function [time, TEMP] = house_cooling()
     temp = 290;
     A = 6 * 10^2;  %m^2
     d = 0.25; %m
-    k = 0.5; %W/m
+    k = 0.005; %W/m
     
     m = A * d * 2300;  %using 2300 as density
     c = 1000; %for concreat
-    H = 50000; %Watts
+    H = 15000; %Watts
     
     U_0 = c*m*temp;
     
     
-    [time, energy] = ode45(@rate, [1:0.1:(100*60*60)], U_0);
+    %[time, energy] = ode45(@rate, [0:(200*60*60)], U_0);
+  
+    
+    %for i=1:10:200*60*60
+        
     
     TEMP = energyToTemperature(energy, m, c);
     
@@ -28,7 +32,12 @@ function [time, TEMP] = house_cooling()
 
     function [dUdt] = rate(t, U)
         temp = energyToTemperature(U, m, c);
-        heater_state = get_heater_state(temp, set_temp, heater_state);
+        %heater_state = get_heater_state(temp, set_temp, heater_state);
+        if temp > set_temp
+            heater_state = false;
+        else
+            heater_state = true;
+        end
         R_tot = (d / (A*k));
         
         [dUdt] = (H*heater_state) - ((temp - temp_amb) / R_tot);
